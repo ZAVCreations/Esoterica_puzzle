52f2987f9cad7b1a34b7e442374be6603e374411
Nishant Zaveri <dev.zavcreations@gmail.com>
2025-07-18 17:57:20 -0500

    updated source

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,17 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+      [
+        'module-resolver',
+        {
+          alias: {
+            'react-native': 'react-native-web',
+          },
+        },
+      ],
       // react-native-reanimated/plugin must be the last plugin
       'react-native-reanimated/plugin',
     ],
